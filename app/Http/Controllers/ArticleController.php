d56bec40a1db73df0469179e03206550b26b7122
ryo-taaa <ryo.ta70600@gmail.com>
2024-12-13 13:31:06 +0900

    First commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function showList() {
+        $model = new Article();
+        $articles = $model->getList();
+
+        return view('list', ['articles' => $articles]);
+    }
+}
